7985be56c6e0506283f42464e834e8b301797ae1
Elias Nygren <elias.nygren@outlook.com>
2014-03-02 23:30:58 +0200

    models ja course_db

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,5 +1,5 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('db/test.db', sqlite3.OPEN_READONLY);
+var db = new sqlite3.Database('db/course_db/test.db', sqlite3.OPEN_READONLY);
 
 exports.index = function (req, res) {
   res.render('tasks/index.jade', {error: null, cols: [], rows: []});
