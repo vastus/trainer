800da30e8102679687707e1026d0497c8cb9a02f
Service <srv@elserver.domain.com>
2014-03-03 16:45:24 +0200

    from upcloud

--- magic.sh
+++ magic.sh
@@ -2,3 +2,8 @@
 
 cd /home/srv/trainer
 git pull
+
+#with this script you can use continuous integration
+#run the following in  your node.js project
+#npm install supervisor -g
+#nohup supervisor app.js & exit
