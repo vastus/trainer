b5e52f5b2c8c72e974c5e1841722e5eca733b9ee
Juho H <juhoth@ovi.com>
2014-03-03 15:52:26 +0200

    routes for executing and new task

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,13 @@
 module.exports = function (app, routes, tasks) {
-  app.get('/', routes.index);
+    app.get('/', routes.index);
 
-  /**
-   * Routes for tasks.
-   */
-  app.get('/tasks', tasks.index);
-  app.post('/tasks', tasks.execute);
+    /**
+     * Routes for tasks.
+     */
+    app.get('/tasks/:id', tasks.showTask);
+    app.get('/tasks/:id/execute', tasks.executeTask);
+
+    app.get('/tasks/new', tasks.newTask);
+    // app.post('/tasks', tasks.createTask);
 };
 
