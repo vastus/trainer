9637e9e73bdbece9db3e45ddb3680e2ecdd9e85f
Juho H <juhoth@ovi.com>
2014-03-04 18:28:55 +0200

    add execute method for database model

--- models/database.js
+++ models/database.js
@@ -7,7 +7,29 @@ var schema = new Schema({
     sql: { type: String, default: '' },
 });
 
-schema.methods = {};
+schema.methods = {
+    execute: function execute(query, cb) {
+                 console.log('in schema exec');
+        var sqlite3 = require('sqlite3').verbose();
+        var db = new sqlite3.Database('db/course_db/' + this.name + '.db');
+
+        db.serialize(function() {
+            console.log('in serialize');
+            db.run(query);
+            db.close();
+            return cb(null);
+            //db.all(query, function(err, rows) {
+            //    console.log(query);
+            //    if (err) return { error: err, cols: null, rows: null };
+            //    var cols = [];
+            //    for (var col in rows[0]) {
+            //        cols.push(col);
+            //    }
+            //    return { error: null, cols: cols, rows: rows };
+            //});
+        });
+    }
+};
 
 module.exports = mongoose.model('Database', schema);
 
