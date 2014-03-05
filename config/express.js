262d6a18f2ceab994a7d4ba3baef8e2f08a082d5
Juho H <juhoth@ovi.com>
2014-03-05 19:13:34 +0200

    introduce current user middleware

--- config/express.js
+++ config/express.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var helpers = require('../controllers/helpers');
 
 /**
  * Express settings
@@ -7,23 +8,25 @@ var path = require('path');
  */
 
 module.exports = function (app, dirname) {
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', path.join(dirname, 'views'));
-  app.set('view engine', 'jade');
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', path.join(dirname, 'views'));
+    app.set('view engine', 'jade');
 
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.json());
-  app.use(express.urlencoded());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(require('stylus').middleware(path.join(dirname, 'public')));
-  app.use(express.static(path.join(dirname, 'public')));
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session());
+    app.use(helpers.currentUser);
+    app.use(app.router);
+    app.use(require('stylus').middleware(path.join(dirname, 'public')));
+    app.use(express.static(path.join(dirname, 'public')));
 
-  if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-  }
+    if ('development' == app.get('env')) {
+        app.use(express.errorHandler());
+    }
 
-}
+};
+
